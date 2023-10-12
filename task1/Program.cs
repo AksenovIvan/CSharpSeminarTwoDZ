71431a76a16cdf48a2fb45ff3417e93427ae96ae
AksenovIvan <axenovivan7@gmail.com>
2023-10-12 14:43:10 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int readint(string msg)
+{
+    System.Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int number;
+    number = int.Parse(numb);
+    return number;
+}
+int number = readint("Введите трехзначное число ->");
+if (number < 1000 && number > 99)
+{
+    int twoNumbers = number / 10 % 10;
+    System.Console.WriteLine(twoNumbers);
+    
+}
+else System.Console.WriteLine("Вы ввели неправильное число");
