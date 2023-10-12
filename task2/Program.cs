60bba7aa8336403ff730f2bc04d2b1e54afb136f
AksenovIvan <axenovivan7@gmail.com>
2023-10-12 21:48:33 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет. Не использовать строки для расчета.
+using System.Globalization;
+
+int readint(string msg)
+{
+    System.Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int number;
+    number = int.Parse(numb);
+    return number;
+}
+int GetThirdRank(int input)
+{
+    while (input > 999)
+    {
+        input = input / 10;
+    }
+    return input % 10;
+}
+
+bool ValidateNumber (int input)
+{
+    if (input < 100)
+    {
+        System.Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+int input = readint ("Введите число >");
+if (ValidateNumber(input))
+{
+    System.Console.WriteLine(GetThirdRank(input));
+}
---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int readint(string msg)
+{
+    System.Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int number;
+    number = int.Parse(numb);
+    return number;
+}
+int number = readint("Введите число обозначающее день недели (от 1 до 7)->");
+if (number == 1)
+{
+System.Console.WriteLine("Будний день");
+}
+else
+System.Console.WriteLine("Выходной день");
